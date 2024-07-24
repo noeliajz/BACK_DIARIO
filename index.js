9a0a7240ddbf2628326578fbf2053575c4e51eda
noeliajudithzelaya <noeliajudithzelayautnfrt@gmail.com>
2024-07-24 02:31:20 -0300

    se terminaron las configuraciones iniciales

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import morgan from 'morgan';
+import path from 'path'
 
 const app = express();
 
@@ -9,3 +11,7 @@ app.listen( app.get('port'), ()=>{
 })
 
 app.use(cors());
+app.use(express.json())
+app.use(morgan('dev'))
+console.log(__dirname)
+app.use(express.static(path.join(__dirname, '/public')))
