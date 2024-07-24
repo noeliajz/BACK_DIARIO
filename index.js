e7159f7d4a6ccd56d4592573ee711bca1327b8c9
noeliajudithzelaya <noeliajudithzelayautnfrt@gmail.com>
2024-07-24 04:16:05 -0300

    realice la conexion a la bd

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import path from 'path'
-import express from 'express'
 import 'dotenv/config'
+import './src/database/dbConection'
 
 const app = express();
 
-app.set('port', process.env.PORT|| 4000)
+app.set('port', process.env.PORT|| 8080)
 app.listen( app.get('port'), ()=>{
     console.log('Estoy en el puerto '+app.get('port'))
 })
@@ -15,6 +15,6 @@ app.listen( app.get('port'), ()=>{
 app.use(cors());
 app.use(express.json())
 app.use(morgan('dev'))
-console.log(__dirname)
+/* console.log(__dirname) */
 app.use(express.static(path.join(__dirname, '/public')))
 
