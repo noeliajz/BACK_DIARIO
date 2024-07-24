941295bb8902aa5cb6e2a6e41b93ae58a6c11293
noeliajudithzelaya <noeliajudithzelayautnfrt@gmail.com>
2024-07-24 01:48:43 -0300

    se agrego la estructura y configuracion babel

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-import express from 'express'
+import express from 'express';
+
+const app = express();
+
+app.set('port', process.env.PORT|| 4000)
+app.listen( app.get('port'), ()=>{
+    console.log('Estoy en el puerto '+app.get('port'))
+})
