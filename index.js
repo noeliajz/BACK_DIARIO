ce9c959abb57ab37bcc2e3d3b7b64cff7313399e
noeliajudithzelaya <noeliajudithzelayautnfrt@gmail.com>
2024-07-24 01:55:17 -0300

    se agrego un middleware

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 
@@ -6,3 +7,5 @@ app.set('port', process.env.PORT|| 4000)
 app.listen( app.get('port'), ()=>{
     console.log('Estoy en el puerto '+app.get('port'))
 })
+
+app.use(cors());
